00b0bb170ad33affa40150e91908139b78d5f79d
Valentin Vetter <vetter.valentin@gmail.com>
2016-11-13 18:29:02 +0100

    feat(subz-hero): Call each subtitles provider sequentially

--- src/subz-hero.js
+++ src/subz-hero.js
@@ -1,11 +1,28 @@
+var Promise = require('bluebird');
 var subdb = require('./providers/subdb');
 var subscene = require('./providers/subscene');
 
-subscene
-  .downloadSubtitles('D:/Films/Old Boy/Old Boy.mkv')
-  .then(function(subtitles) {
-    console.log(subtitles);
-  })
-  .catch(function(error) {
-    console.error(error.message);
-  });
+module.exports = {
+  downloadSubtitles: downloadSubtitles
+};
+
+const SUBTITLES_PROVIDERS = [
+  subdb,
+  subscene
+];
+
+/**
+ * Download subtitles for the given file.
+ * <p>
+ * Call each provider one by one until we actually find subtitles.
+ *
+ * @param {string} file - path to a file
+ * @returns {Promise<string>} the subtitles, formatted as .srt
+ */
+function downloadSubtitles(file) {
+  return SUBTITLES_PROVIDERS.reduce(function(sequencePromise, provider) {
+    return sequencePromise.catch(function() {
+      return provider.downloadSubtitles(file);
+    });
+  }, Promise.reject());
+}
