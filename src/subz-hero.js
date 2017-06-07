2772d1671be8a7e5b329ce3e7aa81c0d82338df4
Valentin Vetter <vetter.valentin@gmail.com>
2017-06-08 00:15:54 +0200

    feat(subz-hero): Add `downloadSubtitles`
    To download a .srt file from a video file.

--- src/subz-hero.js
+++ src/subz-hero.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
 const opensubtitles = require('./providers/opensubtitles');
 const subdb = require('./providers/subdb');
 const subscene = require('./providers/subscene');
@@ -5,7 +9,7 @@ const subscene = require('./providers/subscene');
 const SUBTITLES_PROVIDERS = [
   subdb,
   opensubtitles,
-  subscene
+  subscene,
 ];
 
 /**
@@ -26,6 +30,31 @@ async function getSubtitles(file) {
   }
 }
 
+
+/**
+ * Download subtitles for the given file and create a '.srt' file next to it,
+ * with the same name.
+ * 
+ * @param {string} file - path to a file
+ * @returns {Promise<string>} path to the .srt file
+ */
+async function downloadSubtitles(file) {
+  const subtitles = await getSubtitles(file);
+
+  const { dir, name } = path.parse(file);
+
+  const subtitlesFile = path.format({
+    dir,
+    name,
+    ext: '.srt'
+  });
+
+  await fs.writeFileAsync(subtitlesFile, subtitles);
+
+  return subtitlesFile;
+}
+
 module.exports = {
+  downloadSubtitles,
   getSubtitles
 };
