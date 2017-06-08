0e84179534faf1da3e908215bf1fed4c2457eaa9
Valentin Vetter <vetter.valentin@gmail.com>
2017-06-08 22:40:24 +0200

    feat: Add `subz-hero` script

---
+++ bin/subz-hero.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const subzHero = require('../src/subz-hero');
+
+const args = commander.parse(process.argv).args;
+
+args.forEach(subzHero.downloadSubtitles);
