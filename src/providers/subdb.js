e128069ee8320a0635da62966d38b3218e361aec
Valentin Vetter <vetter.valentin@gmail.com>
2017-06-17 15:22:23 +0200

    refactor(subdb): Inline method
    Motivation: the `hash` module was specific to SubDB. It makes more sense to have the custom hash logic in the `subdb` module, and simply extract a `fileUtil` module instead.

--- src/providers/subdb.js
+++ src/providers/subdb.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto');
+const Promise = require('bluebird');
 const request = require('request-promise');
-const hash = require('../utils/hash');
+const fileUtil = require('../util/file-util');
 
 const SUBDB_API_URL = 'http://api.thesubdb.com';
 
@@ -10,9 +12,9 @@ const SUBDB_API_URL = 'http://api.thesubdb.com';
  * @returns {Promise<string>} the subtitles, formatted as .srt
  */
 async function getSubtitles(file) {
-  const digest = await hash.computeHash(file);
+  const hash = await computeHash(file);
 
-  return getSubtitlesByHash(digest);
+  return getSubtitlesByHash(hash);
 }
 
 /**
@@ -26,14 +28,47 @@ async function getSubtitlesByHash(hash) {
     method: 'GET',
     uri: SUBDB_API_URL,
     qs: {
+      hash,
       action: 'download',
-      hash: hash,
       language: 'en'
     },
     headers: {
       'User-Agent': 'SubDB/1.0 (subz-hero/0.1; https://github.com/BeLi4L/subz-hero)'
     }
   });
+}
+
+/**
+ * Create the hash used to identify a file.
+ *
+ * @param {string} file - path to a file
+ * @returns {Promise<string>} a hex string representing the MD5 digest of the
+ *                            first 64kB and the last 64kB of the given file
+ */
+async function computeHash(file) {
+  const filesize = await fileUtil.getFileSize(file);
+
+  const chunkSize = 64 * 1024;
+
+  const firstBytesPromise = fileUtil.readBytes({
+    file,
+    chunkSize,
+    start: 0,
+  });
+
+  const lastBytesPromise = fileUtil.readBytes({
+    file,
+    chunkSize,
+    start: filesize - chunkSize,
+  });
+
+  const [ firstBytes, lastBytes ] = await Promise.all([firstBytesPromise, lastBytesPromise]);
+
+  return crypto
+    .createHash('md5')
+    .update(firstBytes)
+    .update(lastBytes)
+    .digest('hex');
 }
 
 module.exports = {
---
+++ src/util/file-util.js
@@ -0,0 +1,42 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
+/**
+ * Retrieve the size of a file.
+ *
+ * @param {string} file - path to a file
+ * @returns {Promise<number>} the size of the given file
+ */
+async function getFileSize(file) {
+  const { size } = await fs.statAsync(file);
+  return size;
+}
+
+/**
+ * Read `chunkSize` bytes from the given `file`, starting from byte number `start`.
+ *
+ * @param {string} file      - path to a file
+ * @param {number} start     - byte to start reading from
+ * @param {number} chunkSize - number of bytes to read
+ * @returns {Promise<Buffer>}
+ */
+async function readBytes({ file, start, chunkSize }) {
+  const buffer = new Buffer(chunkSize);
+
+  const fileDescriptor = await fs.openAsync(file, 'r');
+
+  await fs.readAsync(
+    fileDescriptor,
+    buffer,          // buffer to write to
+    0,               // offset in the buffer to start writing at
+    chunkSize,       // number of bytes to read
+    start            // where to begin reading from in the file
+  );
+
+  return buffer;
+}
+
+module.exports = {
+  getFileSize,
+  readBytes,
+};
