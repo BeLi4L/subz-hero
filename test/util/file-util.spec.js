395a363c9aad4cae2295ea2725a4e093e2be6b30
Valentin Vetter <vetter.valentin@gmail.com>
2018-02-10 17:37:05 +0100

    tests: use Jest instead of Mocha+Chai
    Because it's simpler, and it's only 1 lib instead of 3

--- test/util/file-util.spec.js
+++ test/util/file-util.spec.js
@@ -1,31 +1,25 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
 const path = require('path');
 const fileUtil = require('../../src/util/file-util');
-
-chai.use(chaiAsPromised);
-
-const expect = chai.expect;
 
 const testfile = path.resolve(__dirname, 'test-file.txt');
 const testfileSize = 119;
 
 describe('file-util', () => {
 
-  describe('getFileSize()', () => {
+  describe('#getFileSize', () => {
 
     it('should return the size of the given file', async () => {
       const size = await fileUtil.getFileSize(testfile);
-      expect(size).to.eql(testfileSize);
+      expect(size).toBe(testfileSize);
     });
 
-    it('should throw an error if the file does not exist', () => {
-      expect(fileUtil.getFileSize('missing_file')).to.be.rejected;
+    it('should throw an error if the file does not exist', async() => {
+      await expect(fileUtil.getFileSize('missing_file')).rejects.toThrow();
     });
 
   });
 
-  describe('readBytes()', () => {
+  describe('#readBytes', () => {
 
     it('should return a full chunk when (start + chunkSize < filesize)', async () => {
       const buffer = await fileUtil.readBytes({
@@ -34,9 +28,9 @@ describe('file-util', () => {
         chunkSize: 38
       });
       const expectedBuffer = Buffer.from('This file is used by file-util.spec.js');
-      expect(buffer).to.be.an.instanceOf(Buffer);
-      expect(buffer).to.be.of.length(38);
-      expect(buffer).to.eql(expectedBuffer);
+      expect(buffer).toBeInstanceOf(Buffer);
+      expect(buffer).toHaveLength(38);
+      expect(buffer).toEqual(expectedBuffer);
     });
 
     it('should return a short chunk when (start < filesize <= start + chunkSize)', async () => {
@@ -46,9 +40,9 @@ describe('file-util', () => {
         chunkSize: 10000
       });
       const expectedBuffer = Buffer.from('impact file-util tests.');
-      expect(buffer).to.be.an.instanceOf(Buffer);
-      expect(buffer).to.be.of.length(23);
-      expect(buffer).to.eql(expectedBuffer);
+      expect(buffer).toBeInstanceOf(Buffer);
+      expect(buffer).toHaveLength(23);
+      expect(buffer).toEqual(expectedBuffer);
     });
 
     it('should return an empty chunk when (filesize <= start)', async () => {
@@ -58,18 +52,18 @@ describe('file-util', () => {
         chunkSize: 10000
       });
       const expectedBuffer = Buffer.alloc(0);
-      expect(buffer).to.be.an.instanceOf(Buffer);
-      expect(buffer).to.be.of.length(0);
-      expect(buffer).to.eql(expectedBuffer);
+      expect(buffer).toBeInstanceOf(Buffer);
+      expect(buffer).toHaveLength(0);
+      expect(buffer).toEqual(expectedBuffer);
     });
 
-    it('should throw an error if the file does not exist', () => {
+    it('should throw an error if the file does not exist', async () => {
       const promise = fileUtil.readBytes({
         file: 'missing_file',
         start: 0,
         chunkSize: 10
       });
-      expect(promise).to.be.rejected;
+      await expect(promise).rejects.toThrow();
     });
 
   });
