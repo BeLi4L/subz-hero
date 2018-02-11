5696356376c3c008ac75a785d0d4e8395ebe54cf
Valentin Vetter <vetter.valentin@gmail.com>
2018-02-11 16:46:38 +0100

    feat(cli): walk through directories

--- cli/subz-hero.js
+++ cli/subz-hero.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 
 const commander = require('commander')
+const fs = require('fs-extra')
+const fileUtil = require('../src/util/file-util')
 const subzHero = require('../src/subz-hero')
+
+console.log('SUBZ-HERO')
+console.log('=========')
 
 const args = commander.parse(process.argv).args
 
-args.forEach(subzHero.downloadSubtitles)
+args.forEach(downloadSubtitles)
+
+async function downloadSubtitles (path) {
+  if (await fileUtil.isDirectory(path)) {
+    const files = await fs.readdir(path)
+
+    // TODO: DL by batches, to prevent too many API requests
+    files.forEach(subzHero.downloadSubtitles)
+  } else {
+    await subzHero.downloadSubtitles(path)
+  }
+}
